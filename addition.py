a7474ceb2f0f0a145da80e8c8d84a2f73f5a64f9
atikahkhonsa <atikahkhonsa@gmail.com>
2024-06-24 10:26:11 +0700

    project0-ai

---
+++ addition.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    a = int(input("Masukkan nilai A : "))
+    b = int(input("Masukkan nilai B : "))
+    hasil = a + b
+    return a + b
+
+    print("Hasil A dan B adalah : ")
+    print(hasil)
---
+++ projectParams.py
@@ -0,0 +1,4 @@
+STUDENT_CODE_DEFAULT = 'addition.py,buyLotsOfFruit.py,shopSmart.py,shopAroundTown.py'
+PROJECT_TEST_CLASSES = 'tutorialTestClasses.py'
+PROJECT_NAME = 'Project 0: Tutorial'
+BONUS_PIC = False
